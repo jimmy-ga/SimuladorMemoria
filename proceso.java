506c7f88582b29f285b5aea01612ce45f59f4d6c
Jimmy Granados Abarca <ji.granabar@gmail.com>
2015-05-14 19:08:54 -0600

    Clase proceso con función main de prueba

---
+++ proceso.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class proceso
+{
+	//Clase proceso. Atributos id, nombre, memoria (requerida).
+	//Funciones: constructor, set de cada atributo y get de cada atributo.
+
+	private int id;
+	private String nombre;
+	private int memoria;
+
+	public proceso(int i,String n,int m)//Constructor con parametros de entrada id,nombre,memoria requerida
+	{
+		this.id=i;
+		this.nombre=n;
+		this.memoria=m;
+	}
+
+	public int getId()//Obtiene el id del proceso
+	{
+		return this.id;
+	}
+
+	public String getName()//Obtiene el nombre del proceso
+	{
+		return this.nombre;
+	}
+
+	public int getMem()//Obtiene la memoria requerida del proceso
+	{
+		return this.memoria;
+	}
+
+	public void setId(int i)//Cambia o establece el id del proceso
+	{
+		this.id=i;
+	}
+
+	public void setName(String n)//Cambia o establece el nombre del proceso
+	{
+		this.nombre=n;
+	}
+
+	public void setMem(int m)//Cambia o establece la memoria requerida del proceso
+	{
+		this.memoria=m;
+	}
+
+	public String getDatos()//Devuelve un string con todos los atributos del proceso
+	{
+		return "Id proceso: "+String.valueOf(this.id)+" Nombre: "+this.nombre+" Memoria requerida: "+String.valueOf(this.memoria);
+	}
+
+	public static void main(String[] args)//Main temporal para probar las funciones de creacion y obtencion de procesos
+	{
+		Random  rnd = new Random();
+		ArrayList<proceso> lista = new ArrayList<proceso>();
+
+		for(int i=0;i<10;i++)
+		{
+			lista.add(new proceso(i,"proceso"+String.valueOf(i),rnd.nextInt(1024)+1));
+		}
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(lista.get(i).getDatos());
+		}
+	}
+}
